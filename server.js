38310ebd2db67e1fc7fb4973853d08e5a40bb27d
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-06-05 11:03:45 -0700

    added get requests

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
